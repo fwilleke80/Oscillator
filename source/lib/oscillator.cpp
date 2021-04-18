1e4f4af0f5656c042412acee8be126901d46c986
Frank Willeke <frank@frankwilleke.de>
2021-04-18 23:34:36 +0200

    Finished waveform drawing; minor fixes

--- source/lib/oscillator.cpp
+++ source/lib/oscillator.cpp
@@ -83,7 +83,7 @@ Float Oscillator::GetTriangle(Float x, const WaveformParameters& parameters) con
 
 Float Oscillator::GetSquare(Float x, const WaveformParameters& parameters) const
 {
-	Float result = Sin(FreqToAngularVelocity(x)) > 0.0 ? 0.0 : 1.0;
+	Float result = Sign(Sin(FreqToAngularVelocity(x)));
 
 	if (parameters.invert)
 		result = 1.0 - result;
@@ -134,7 +134,7 @@ Float Oscillator::GetAnalogSaw(Float x, const WaveformParameters& parameters) co
 
 	result *= TWOBYPI;
 
-	if (parameters.invert)
+	if (!parameters.invert)
 		result *= -1.0;
 
 	if (parameters.valueRange == VALUERANGE::RANGE01)
@@ -156,26 +156,38 @@ BaseBitmap* Oscillator::RenderToBitmap(Int32 w, Int32 h, Oscillator::OSCTYPE osc
 	}
 
 	// Background
-	bmp->Clear(20, 20, 20);
+	bmp->Clear(32, 32, 32);
 
 	// Grid
+	bmp->SetPen(8, 48, 8);
 	if (parameters.valueRange == Oscillator::VALUERANGE::RANGE11)
 		bmp->Line(0, h / 2, w - 1, h / 2);
 	else
 		bmp->Line(0, h - 1, w - 1, h - 1);
 	bmp->Line(0, 0, 0, h - 1);
 
+	// Waveform
+	bmp->SetPen(32, 255, 16);
 	static const Float previewScaleX = 2.0;
 	const Float iw1 = Inverse((Float)(w - 1));
-
+	Int32 yPrevious = NOTOK;
 	for (Int32 x = 0; x < w; ++x)
 	{
 		const Float xSample = (Float)x * iw1 * previewScaleX;
 		Float y = (Int32)(SampleWaveform(xSample, oscType, parameters) * (Float)(h - 1));
+		if (oscType == Oscillator::OSCTYPE::SAW_ANALOG)
+			y *= 0.9;
 		if (parameters.valueRange == Oscillator::VALUERANGE::RANGE11)
 			y = y * 0.5 + h * 0.5;
 
-		bmp->SetPixel(x, h - 1 - (Int32)y, 0, 255, 0);
+		const Int32 yDraw = h - 1 - (Int32)y;
+
+		if (Abs(yDraw - yPrevious) > 1 && x > 0)
+			bmp->Line(x - 1, yPrevious, x, yDraw);
+		else
+			bmp->SetPixel(x, yDraw, 0, 255, 0);
+
+		yPrevious = yDraw;
 	}
 
 	return bmp;
